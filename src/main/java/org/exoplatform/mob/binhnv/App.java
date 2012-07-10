a124d8e6b1a6952b194cee09ee8dfc4cc94166cc
BinhNV <binhnv@exoplatform.com>
2012-07-10 10:29:53 +0700

    commit jcr-sample project

---
+++ src/main/java/org/exoplatform/mob/binhnv/App.java
@@ -0,0 +1,13 @@
+package org.exoplatform.mob.binhnv;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
